6ec502d96fc939d5e922cea7d934076539eab4dd
Christopher Baker <sabbathia@gmail.com>
2017-11-27 12:40:41 -0600

    Now generates icons with hover text details

---
+++ MVCItemGen/wwwroot/js/generate.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+
+var g = document.getElementById("ResultsTable");
+
+g.addEventListener("click", function () { alert("test") });
+
+//var h = document.getElementsByClassName("ItemTables");
+
+
+//for (var i = 0; i < h.length; i++) {
+//h.item.addEventListener("click", function () { alert("testing 2") });
+//}
+
+
